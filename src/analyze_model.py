e23b7d5447ce50dd944c0a686b2f88b235a582b3
BWallyn <ben96.wallyn@gmail.com>
2023-08-29 23:22:11 +0200

    function to get unsold rows

--- src/analyze_model.py
+++ src/analyze_model.py
@@ -12,9 +12,32 @@ import pandas as pd
 # ===================
 
 def get_soldout_rows(df: pd.DataFrame, pred: np.array, target: np.array) -> pd.DataFrame:
-    """
+    """Get the rows with soldouts (ventes >= fourni)
+
+    Args:
+        df: dataset
+        pred: predictions of the model (newspapers delivered)
+        target: target (sales)
+    Returns:
+        df_soldout: dataset of the sold out rows
     """
     df['ventes'] = target
     df['fourni'] = pred
     df_soldout = df[df["ventes"] >= df["fourni"]]
     return df_soldout
+
+
+def get_unsold_rows(df: pd.DataFrame, pred: np.array, target: np.array) -> pd.DataFrame:
+    """Get the rows with unsold newspapers (ventes < fourni)
+
+    Args:
+        df: dataset
+        pred: predictions of the model (newspapers delivered)
+        target: target (sales)
+    Returns:
+        df_unsold: dataset of the unsold rows
+    """
+    df['ventes'] = target
+    df['fourni'] = pred
+    df_unsold = df[df["ventes"] < df["fourni"]]
+    return df_unsold
